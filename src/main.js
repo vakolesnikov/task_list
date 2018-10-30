e05372a2fb90b5c199270a13a5a5ead98815adeb
Vasiliy Kolesnikov <v.a.kolesnikov@bk.ru>
2018-10-31 02:21:05 +0300

    vue-router added

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App';
 
 Vue.config.productionTip;
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+   routes: [
+       {path: '/main', component: App},
+   ]
+});
+
+
+// new Vue({
+//     render: h => h(App)
+// }).$mount('#app');
 
 new Vue({
-    render: h => h(App)
-}).$mount('#app');
+    router
+}).$mount('#app')
+
